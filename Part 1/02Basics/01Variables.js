cf349ed92546b2479076b801f6dfd386657f62b2
Hency Dsouza <hencyhd39@gmail.com>
2024-04-16 13:01:30 +0530

    Part one

---
+++ Part 1/02Basics/01Variables.js
@@ -0,0 +1,16 @@
+var fullName = "Hency Jostan Dsouza";
+
+var courseName = "React JS Bootcamp";
+
+var isLoggedIn = false;
+
+var loggedCount = 34;
+
+// console.log(loggedCount);
+// console.log("loggedCount");
+
+var paymentMode;
+
+paymentMode = "Credit Card";
+
+console.log(paymentMode);
