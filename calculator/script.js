e4944d62955cc6e4485d5b9f9698e03c55a26324
Hency Dsouza <hencyhd39@gmail.com>
2024-05-07 14:03:01 +0530

    calculator updated

--- calculator/script.js
+++ calculator/script.js
@@ -1,4 +1,14 @@
 let memory = 0;
+let history = []
+const histryEle = document.getElementById('history')
+
+function saveData(arr) {
+    localStorage.setItem('history', JSON.stringify(arr))
+}
+
+function loadData() {
+    return JSON.parse(localStorage.getItem('history'))
+}
 
 function addToDisplay(value) {
     const display = document.getElementById("display");
@@ -8,8 +18,12 @@ function addToDisplay(value) {
 function calculate() {
     const display = document.getElementById("display");
     try {
+        const query = display.value
         const result = eval(display.value);
         display.value = result;
+        history.push(query + "=" + result)
+        saveData(history)
+        displayHistory()
         // memory = result;
     } catch (error) {
         display.value = "Error";
@@ -55,3 +69,27 @@ function memoryRecall() {
 function memoryClear() {
     memory = "";
 }
+
+function clearStorage() {
+    localStorage.setItem('history', '')
+    history = loadData()
+    histryEle.innerHTML = ""
+    location.reload()
+}
+
+document.getElementById('clear').addEventListener('click', () => {
+    clearStorage()
+})
+
+function displayHistory() {
+    histryEle.innerHTML = ""
+    for (let i = 0; i < history.length; i++) {
+        histryEle.innerHTML += `<li>${history[i]}</li>`
+    }
+}
+
+(function () {
+    history = loadData() ? loadData() : []
+    displayHistory()
+    console.log(history)
+})()
